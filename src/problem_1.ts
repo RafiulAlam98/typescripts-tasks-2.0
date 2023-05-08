3599c12841a3ea894aaa62c999074ae972036c4e
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-05-08 08:23:51 +0600

    problem_1 commit

---
+++ src/problem_1.ts
@@ -0,0 +1,7 @@
+const defaultParameters = (param1:string, param2:number):void =>{
+        for (let i:number = 1;i<=param2; i++){
+                console.log(param1)
+        }
+}
+
+defaultParameters("I Love Programming Hero", 90)
