721c84b2c51c38b4b76da60fcdf4a6abe88fd3e6
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-05-09 08:38:22 +0600

    problem_5 added

---
+++ src/problem_5.ts
@@ -0,0 +1,15 @@
+const myFunc = (param:unknown):string =>{
+        if (typeof param === "string"){
+                return `${param}`
+        }
+        else{
+                return `Error! this is not a string`
+        }
+}
+
+type StringCheck= string
+const stringCheck:StringCheck = "Tonmoy"
+type StringCheck1= number
+const stringCheck1:StringCheck1 = 3
+const output = myFunc(stringCheck1)
+console.log(output)
